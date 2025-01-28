ee4ca88b6b53e78894f1325e709d14d0a115bc72
lotlouischoitslab <louis.s.cho@gmail.com>
2025-01-27 19:33:17 -0600

    fixed code

--- main.py
+++ main.py
@@ -6,19 +6,6 @@ import math
 from scipy.stats import norm
 import os
 
-# IDs for different types of Vehicles extracted before
-# I395_A = [[694, 1], [1416, 1], [1779, 1], [2342, 1], [2817, 1], [3330, 1], [3758, 1], [4261, 1], [4715, 1], [5385, 1], [5741, 1], [6398, 1], [7299, 1], [7832, 1], [8171, 1], [8692, 1], [8885, 1], [9409, 1], [9505, 1], [10150, 1], [10182, 1]]
-# I395_S = [(7128, 1), (2364, 1), (1635, 1), (4138, 1), (3890, 1), (1333, 1), (5680, 1), (9553, 1), (9388, 1), (9454, 1), (4519, 1), (2662, 1), (4586, 1), (5289, 1), (1108, 1), (9914, 1), (7571, 1), (2874, 1), (9231, 1), (9229, 1), (5281, 1)]
-# I395_L = [(532, 1), (638, 1), (7082, 1), (4386, 1), (10022, 1), (4579, 1), (8194, 1), (9008, 1), (9230, 1), (6328, 1), (7804, 1), (6901, 1), (7581, 1), (7581, 1), (6901, 1), (7731, 1), (3450, 1), (8412, 1), (8884, 1), (7334, 1), (5284, 1), (8029, 1), (9208, 1), (7731, 1)]
-# I9094_A = [[5366, 1], [195, 2], [286, 3]]
-# I9094_S = [(3799, 1), (174, 2), (232, 3)]
-# I9094_L = [(5012, 1), (5266, 1), (442, 3)]
-# I294l1_A = [[8, 1], [9, 1], [12, 1], [33, 3], [40, 3], [41, 3], [3, 7], [11, 7], [17, 7], [51, 8], [62, 8], [65, 8], [24, 9], [28, 9], [30, 9], [19, 11], [22, 11], [35, 11], [18, 19], [25, 19], [48, 20], [50, 20], [54, 20], [13, 21]]
-# I294l1_S = [(727, 7), (5557, 8), (46, 3), (17, 1), (33, 1), (65, 1), (24, 3), (575, 3), (628, 3),(15, 7), (24, 7), (67, 7), (81, 8), (145, 8), (197, 8), (32, 9), (48, 9), (778, 9), (38, 11), (132, 11), (497, 11), (321, 19), (1222, 19),(40, 20),(65, 20),(600, 20),(18, 21)]
-# I294l1_L = [(603, 11), (1115, 20), (4681, 8), (18, 1), (116, 1),(202, 1), (7, 3), (30, 3),(65, 3), (26, 7), (34, 7), (128, 7), (45, 8), (93, 8), (539, 8), (462, 9), (2008, 9), (3062, 9), (12, 11), (39, 11), (121, 11), (4, 19), (108, 19), (55, 20), (806, 20), (1731, 20), (34, 21)]
-# I294l2_A = [[462, 5], [107, 23], [291, 28], [90, 29], [118, 30], [231, 31], [181, 33], [218, 35], [46, 36], [72, 38], [211, 41], [229, 42]]
-# I294l2_S = [[143, 5], [18, 23], [234, 28], [38, 29], [81, 30], [352, 31], [317, 33], [2, 35], [176, 36], [292, 38], [89, 41], [299, 42]]
-# I294l2_L = [(260, 5), (111, 23), (156, 28), (102, 29), (30, 30), (35, 31), (25, 33), (8, 35), (71, 36), (86, 38), (364, 41), (9, 42)]
 
 # Filter out only for ACC models 
 
@@ -33,7 +20,7 @@ Instructions
 
 
 
-def find_leader_data(df, follower_id, run_index):
+def find_leader_data(df, follower_id, run_index,pos):
     global most_leading_leader_id
     
     follower_data = df[(df['id'] == follower_id) & (df['run_index'] == run_index)]
@@ -75,9 +62,9 @@ def find_leader_data(df, follower_id, run_index):
     
     return leader_df
 
-def extract_subject_and_leader_data(df, follower_id, run_index):
+def extract_subject_and_leader_data(df, follower_id, run_index, pos):
     sdf = df[(df['id'] == follower_id) & (df['run_index'] == run_index)].round(2)
-    ldf = find_leader_data(df, follower_id, run_index).round(2)
+    ldf = find_leader_data(df, follower_id, run_index, pos).round(2)
     
     #find the intersection of time frames between leader and subject
     mutual_times = np.intersect1d(ldf['time'], sdf['time'])
@@ -198,7 +185,7 @@ def acceleration_calculator(i, t, vehicle, accl_max, v_desired, Gamma1, Gamma2, 
     return accl_, fn, Wc * fn
 
 
-def simulate_car_following(params):
+def simulate_car_following(params, total_time, time_step, sdf, pos, leader_position, leader_speed, follower_id, accl_max, v_desired, Tcorr, RT, target_position, target_speed):
     global Tmax, Alpha, Beta, Wc, Gamma1, Gamma2, Wm
     Tmax, Alpha, Beta, Wc, Gamma1, Gamma2, Wm = params
     
@@ -225,8 +212,8 @@ def simulate_car_following(params):
         
     return position, speed, acl
 
-def fitness(params):
-    sim_position, sim_speed, acl = simulate_car_following(params)
+def fitness(params, total_time, time_step, sdf, pos, leader_position, leader_speed, follower_id, accl_max, v_desired, Tcorr, RT, target_position, target_speed):
+    sim_position, sim_speed, acl = simulate_car_following(params, total_time, time_step, sdf, pos, leader_position, leader_speed, follower_id, accl_max, v_desired, Tcorr, RT, target_position, target_speed)
     diff_position = np.array(sim_position) - np.array(target_position)
     diff_speed = np.array(sim_speed) - np.array(target_speed)
     
@@ -284,7 +271,7 @@ def fitness(params):
     
     return fitness_value, error_metrics  # Return fitness and all error metrics
 
-    
+
 
 def crossover(parent1, parent2):
     crossover_point = random.randint(0, len(parent1) - 1)
@@ -292,7 +279,8 @@ def crossover(parent1, parent2):
     child2 = parent2[:crossover_point] + parent1[crossover_point:]
     return child1, child2
 
-def mutate(child):
+
+def mutate(child, mutation_rate=0.1):
     for i in range(len(child)):
         if random.random() < mutation_rate:
             child[i] += random.uniform(-0.1, 0.1)
@@ -300,7 +288,7 @@ def mutate(child):
 
 
 
-def genetic_algorithm():
+def genetic_algorithm(population_size, num_generations, total_time, time_step, sdf, pos, leader_position, leader_speed, follower_id, accl_max, v_desired, Tcorr, RT, target_position, target_speed):
     # define parameter ranges for PT model
     Tmax_range = (2, 8.0)
     Alpha_range = (0, 0.6)
@@ -320,7 +308,7 @@ def genetic_algorithm():
     
     for generation in range(num_generations):
         #evaluate fitness and errors
-        fitness_and_errors = [fitness(individual) for individual in population]
+        fitness_and_errors = [fitness(individual, total_time, time_step, sdf, pos, leader_position, leader_speed, follower_id, accl_max, v_desired, Tcorr, RT, target_position, target_speed) for individual in population]
         population_sorted = sorted(zip(population, fitness_and_errors), key=lambda x: x[1][0], reverse=True)
         population = [ind for ind, _ in population_sorted]
         
@@ -343,6 +331,8 @@ def genetic_algorithm():
     
     #return the best individual, best error, and best error metrics after all generations
     return best_individual, best_error, best_metrics
+
+
 
 def plot_simulation(timex, leader_position, target_position, sim_position, leader_speed, target_speed, sim_speed, follower_id, most_leading_leader_id, run_index, save_dir):
     plt.figure(figsize=(10, 12))
@@ -367,15 +357,23 @@ def plot_simulation(timex, leader_position, target_position, sim_position, leade
     plot_filename = os.path.join(save_dir, f'{outname}_FID_{follower_id}_LID_{int(most_leading_leader_id)}_run_{run_index}.png')
     plt.savefig(plot_filename)
     plt.close()
+ 
+
 
 def visualize_parameter_distributions(all_params):
     param_names = ['Tmax', 'Alpha', 'Beta', 'Wc', 'Gamma1', 'Gamma2', 'Wm']
     num_params = len(param_names)
     
-    #convert list of lists into a 2D numpy array for easier column-wise access
+    # Convert list of lists into a 2D numpy array for easier column-wise access
     all_params_array = np.array(all_params)
+    if len(all_params_array.shape) == 1:  # If 1D, wrap it in another list
+        print("Warning: Input array is 1D, reshaping to 2D...")
+        all_params_array = np.expand_dims(all_params_array, axis=0)
     
-    #histograms for each parameter
+    # Debug the shape
+    print("Shape of all_params_array:", all_params_array.shape)
+    
+    # Histograms for each parameter
     fig, axs = plt.subplots(1, num_params, figsize=(20, 4))
     for i in range(num_params):
         axs[i].hist(all_params_array[:, i], bins=20, color='skyblue', edgecolor='black')
@@ -386,7 +384,7 @@ def visualize_parameter_distributions(all_params):
     plt.tight_layout()
     plt.show()
 
-    #create box plots for each parameter
+    # Create box plots for each parameter
     plt.figure(figsize=(10, 6))
     plt.boxplot(all_params_array, labels=param_names, patch_artist=True)
     plt.title('Distribution of PT Model Parameters')
@@ -394,6 +392,7 @@ def visualize_parameter_distributions(all_params):
     plt.xticks(rotation=45)
     plt.tight_layout()
     plt.show()
+
 
 
 # 4 is AV 
@@ -419,23 +418,25 @@ def filter_data(df_key,df_path):
 
 
 def main():
+    # IDs for different types of Vehicles extracted before
+    I395_A = [[694, 1], [1416, 1], [1779, 1], [2342, 1], [2817, 1], [3330, 1], [3758, 1], [4261, 1], [4715, 1], [5385, 1], [5741, 1], [6398, 1], [7299, 1], [7832, 1], [8171, 1], [8692, 1], [8885, 1], [9409, 1], [9505, 1], [10150, 1], [10182, 1]]
+    I395_S = [(7128, 1), (2364, 1), (1635, 1), (4138, 1), (3890, 1), (1333, 1), (5680, 1), (9553, 1), (9388, 1), (9454, 1), (4519, 1), (2662, 1), (4586, 1), (5289, 1), (1108, 1), (9914, 1), (7571, 1), (2874, 1), (9231, 1), (9229, 1), (5281, 1)]
+    I395_L = [(532, 1), (638, 1), (7082, 1), (4386, 1), (10022, 1), (4579, 1), (8194, 1), (9008, 1), (9230, 1), (6328, 1), (7804, 1), (6901, 1), (7581, 1), (7581, 1), (6901, 1), (7731, 1), (3450, 1), (8412, 1), (8884, 1), (7334, 1), (5284, 1), (8029, 1), (9208, 1), (7731, 1)]
+    I9094_A = [[5366, 1], [195, 2], [286, 3]]
+    I9094_S = [(3799, 1), (174, 2), (232, 3)]
+    I9094_L = [(5012, 1), (5266, 1), (442, 3)]
+    I294l1_A = [[8, 1], [9, 1], [12, 1], [33, 3], [40, 3], [41, 3], [3, 7], [11, 7], [17, 7], [51, 8], [62, 8], [65, 8], [24, 9], [28, 9], [30, 9], [19, 11], [22, 11], [35, 11], [18, 19], [25, 19], [48, 20], [50, 20], [54, 20], [13, 21]]
+    I294l1_S = [(727, 7), (5557, 8), (46, 3), (17, 1), (33, 1), (65, 1), (24, 3), (575, 3), (628, 3),(15, 7), (24, 7), (67, 7), (81, 8), (145, 8), (197, 8), (32, 9), (48, 9), (778, 9), (38, 11), (132, 11), (497, 11), (321, 19), (1222, 19),(40, 20),(65, 20),(600, 20),(18, 21)]
+    I294l1_L = [(603, 11), (1115, 20), (4681, 8), (18, 1), (116, 1),(202, 1), (7, 3), (30, 3),(65, 3), (26, 7), (34, 7), (128, 7), (45, 8), (93, 8), (539, 8), (462, 9), (2008, 9), (3062, 9), (12, 11), (39, 11), (121, 11), (4, 19), (108, 19), (55, 20), (806, 20), (1731, 20), (34, 21)]
+    I294l2_A = [[462, 5], [107, 23], [291, 28], [90, 29], [118, 30], [231, 31], [181, 33], [218, 35], [46, 36], [72, 38], [211, 41], [229, 42]]
+    I294l2_S = [[143, 5], [18, 23], [234, 28], [38, 29], [81, 30], [352, 31], [317, 33], [2, 35], [176, 36], [292, 38], [89, 41], [299, 42]]
+    I294l2_L = [(260, 5), (111, 23), (156, 28), (102, 29), (30, 30), (35, 31), (25, 33), (8, 35), (71, 36), (86, 38), (364, 41), (9, 42)]
+
+
     population_size, num_generations, mutation_rate = 40, 80, 0.1  #simulation parameters
     accl_max, v_desired, Tcorr, RT = 3.0, 36.0, 20.0, 0.6 #suggested values from the paper and v_desired=36 is the v_desired from the data
     most_leading_leader_id = None
-
-
-    
-    # datasets = {
-    # "df395": "TGSIM/I395_Trajectories.csv",
-    # "df9094": "TGSIM/I90_94_Moving_Trajectories.csv",
-    # "df294l1": "TGSIM/I294_L1_Trajectories.csv",
-    # "df294l2": "TGSIM/I294_L2_Trajectories.csv"}
-
-    # groups = {
-    #     "df395": ["I395_A", "I395_S", "I395_L"],
-    #     "df9094": ["I9094_L", "I9094_S", "I9094_A"],
-    #     "df294l1": ["I294l1_L", "I294l1_S", "I294l1_A"],
-    #     "df294l2": ["I294l2_L", "I294l2_S", "I294l2_A"]}
+ 
 
     datasets = { 
     "df395": "TGSIM/I395_Trajectories.csv",
@@ -451,34 +452,43 @@ def main():
         "df294l2": ["I294l2_L", "I294l2_S", "I294l2_A"]
     }
 
+ 
 
     #Save directory for plots
     save_dir = 'Results/'
 
     #iterate through each dataset and group
     for df_key, df_path in datasets.items(): 
-        df = filter_data(df_key,df_path)
+        #df = filter_data(df_key,df_path)
+
+        df = pd.read_csv(df_path)
+        df = df.sort_values(by='time')
+        df['time'] = df['time'].round(1)
 
 
-        if True:
-            continue 
+        # if True:
+        #     continue 
 
 
         if df_key == "df395":
             pos = "yloc_kf"
         else:
             pos = "xloc_kf"
+
         for group in groups[df_key]:
-            # Define the current group
-            outname = str("PT_")+str(group)
-            AVs = eval(group)
+      
+            outname = str("PT_")+str(group) 
             all_params = []
-            params_list = []
+            params_list = []    
+            AVs = eval(group)
+
+            df_ids = df['id'].unique()
 
             for data in AVs:
-                follower_id, run_index = data
-                sdf, ldf = extract_subject_and_leader_data(df, follower_id, run_index)
-                print (follower_id)
+                follower_id, run_index = data 
+                sdf, ldf = extract_subject_and_leader_data(df, follower_id, run_index, pos)
+                print(follower_id)
+
                 # Check if sdf is empty
                 if sdf.empty:
                     print(f"No data found for Follower ID {follower_id} and Run Index {run_index}. Skipping...")
@@ -489,13 +499,14 @@ def main():
                     timex = np.linspace(0, total_time, num_steps)
                     leader_position, leader_speed = ldf[pos].tolist(), ldf['speed_kf'].tolist()
                     target_position, target_speed = sdf[pos].tolist(), sdf['speed_kf'].tolist()
-                    best_params, best_error, best_metrics = genetic_algorithm()
+                    best_params, best_error, best_metrics = genetic_algorithm(population_size, num_generations, total_time, time_step, sdf, pos, leader_position, leader_speed, follower_id, accl_max, v_desired, Tcorr, RT, target_position, target_speed)
                     all_params.append(best_params)
                     params_list.append([follower_id, run_index] + best_params + [best_error] + list(best_metrics.values()))
                     #print (params_list)
-                    sim_position, sim_speed, acl = simulate_car_following(best_params)
+                    sim_position, sim_speed, acl = simulate_car_following(params, total_time, time_step, sdf, pos, leader_position, leader_speed, accl_max, v_desired, Tcorr, RT, target_position, target_speed)
                     plot_simulation(timex, leader_position, target_position, sim_position, leader_speed, target_speed, sim_speed, follower_id, most_leading_leader_id, run_index, save_dir)
             
+            print(all_params)
             visualize_parameter_distributions(all_params)
             metrics_names = list(best_metrics.keys())
             columns = ['Follower_ID', 'Run_Index', 'Tmax', 'Alpha', 'Beta', 'Wc', 'Gamma1', 'Gamma2', 'Wm', 'Error'] + metrics_names
